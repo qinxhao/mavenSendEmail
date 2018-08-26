69ddf06c4d55b5af713fd6332ba63ffa5f7d9335
qinhao <qinxhao@126.com>
2018-08-26 23:22:29 +0800

    Initial commit

---
+++ src/main/java/com/qinhao/maven/mavendemo/MailController.java
@@ -0,0 +1,40 @@
+package com.qinhao.maven.mavendemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @RequestMapping("/send")
+
+    public void sendMail(){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("qinxhao@126.com");
+        message.setTo("qinxhao@163.com");
+        message.setSubject("it is a test for spring boot");
+        message.setText("你好，我是秦浩，我正在测试发送邮件。");
+
+        try {
+
+            mailSender.send(message);
+            System.out.println("秦浩的测试邮件已发送。");
+
+        } catch (Exception e) {
+
+            System.out.println( e );
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/qinhao/maven/mavendemo/MavendemoApplication.java
@@ -0,0 +1,15 @@
+package com.qinhao.maven.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
+public class MavendemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavendemoApplication.class, args);
+	}
+}
