4d0820c8e7e3211ed7e31eaf0603c03ce193467c
qinxhao <39058553+qinxhao@users.noreply.github.com>
2020-04-22 22:06:46 +0800

    Update MavendemoApplication.java

--- src/main/java/com/qinhao/maven/mavendemo/MavendemoApplication.java
+++ src/main/java/com/qinhao/maven/mavendemo/MavendemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+//启动类
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableAutoConfiguration
 public class MavendemoApplication {
