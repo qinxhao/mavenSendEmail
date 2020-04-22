65dc75be911a81a688ac1396df3cd5cefe433ebe
qinhao <qinxhao@163.com>
2020-04-22 23:35:15 +0800

    清除

--- src/main/java/com/qinhao/maven/mavendemo/MailController.java
+++ src/main/java/com/qinhao/maven/mavendemo/MailController.java
@@ -6,7 +6,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//qinhaohaha
+
 @RestController
 @RequestMapping("/mail")
 public class MailController {
